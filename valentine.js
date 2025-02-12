013eb56db5736d84ca1c9b08ca481c7eda92b5d1
Smileqiutian <liuxianminxm@gmail.com>
2025-02-12 21:28:50 +0800

    Add files via upload

---
+++ valentine.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const titleElement = document.querySelector(".title");
+const buttonsContainer = document.querySelector(".buttons");
+const yesButton = document.querySelector(".btn--yes");
+const noButton = document.querySelector(".btn--no");
+const catImg = document.querySelector(".cat-img");
+
+const MAX_IMAGES = 5;
+
+let play = true;
+let noCount = 0;
+
+yesButton.addEventListener("click", handleYesClick);
+
+noButton.addEventListener("click", function () {
+    if (play) {
+        noCount++;
+        const imageIndex = Math.min(noCount, MAX_IMAGES);
+        changeImage(imageIndex);
+        resizeYesButton();
+        updateNoButtonText();
+        if (noCount === MAX_IMAGES) {
+            play = false;
+        }
+    }
+});
+
+function handleYesClick() {
+    titleElement.innerHTML = "YEAH！ 答应跟我去约会咯~~~~~";
+    buttonsContainer.classList.add("hidden");
+    changeImage("yes");
+}
+
+function resizeYesButton() {
+    const computedStyle = window.getComputedStyle(yesButton);
+    const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
+    const newFontSize = fontSize * 3.1;
+
+    yesButton.style.fontSize = `${newFontSize}px`;
+}
+
+function generateMessage(noCount) {
+    const messages = [
+        "确定？",
+        "不要拒绝嘛",
+        "想哭了... :(",
+        "心要碎了...",
+        "自闭了...",
+    ];
+
+    const messageIndex = Math.min(noCount, messages.length - 1);
+    return messages[messageIndex];
+}
+
+function changeImage(image) {
+    catImg.src = `img/cat-${image}.jpg`;
+}
+
+function updateNoButtonText() {
+    noButton.innerHTML = generateMessage(noCount);
+}
